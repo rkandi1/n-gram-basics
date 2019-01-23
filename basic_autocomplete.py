10fe8c5b94a9f7e5b84ab539f1c59a1e88e39ec3
Rohan Kandi1 <RohanKandi1@172-18-41-111.DYNAPOOL.NYU.EDU>
2019-01-23 14:28:35 -0500

    working on the model based on the training set

--- basic_autocomplete.py
+++ basic_autocomplete.py
@@ -17,20 +17,21 @@ if __name__ == "__main__":
     unigram_count = dict()
     individual_gram_count = dict()
 
-    with open('cw09b-trec_eval-queries.txt', 'r') as f:
+    with open('files/train_set.txt', 'r') as f:
         for line in f:
-            line = "<s> " + line[:len(line)-1] + " </s>"
+            _, line = line.split('.')
+            line = "<s> " + line[:len(line) - 1] + " </s>"
             line_list = line.split(" ")
             bigram_model.bigram_counter(unigram_count, line_list)
-            bigram_model. individual_word_counter(individual_gram_count, line_list)
+            bigram_model.individual_word_counter(individual_gram_count, line_list)
 
         bigram_model.individual_probabilities(individual_gram_count, unigram_count)
 
     while True:
         query = str(input("Make a query: "))
         isahit = False
+        query = query.split(' ')[0]
         for node in individual_gram_count["<s>"]:
-            query = query.split(" ")[0]
             if node.next == query:
                 sentence = complete_query(query, individual_gram_count)
                 print(sentence)
